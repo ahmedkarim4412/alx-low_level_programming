a6820d0eb6ab5506c7119a7a6b7635ce3a371d3f
ahmedkarim4412 <ahmedkarim4412@gmail.com>
2022-12-09 17:19:34 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -23,14 +23,14 @@ int main(void)
 				putchar(ones);
 				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
 				{
-					putchar(',')
-					putchar(' ')
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
 
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 
