f114183d0707cd2a00cebe3ba0500cdbacfc4e51
ahmedkarim4412 <ahmedkarim4412@gmail.com>
2022-12-13 15:07:27 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to spring
- *
- * Return: On success 1.
- * On error, -1 is retruned, and error is set appropraitly.
- *
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('n');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
