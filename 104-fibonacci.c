de772eb569753eb163d2e19be702c641f3f59778
ahmedkarim4412 <ahmedkarim4412@gmail.com>
2022-12-14 16:05:31 -0800

    104-fibonacci.c

--- 104-fibonacci.c
+++ 104-fibonacci.c
@@ -32,13 +32,14 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", aft1 + (aft2 / l));printf("%lu", aft2 % l);
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
 		bef2 = aft2 - bef2;
 	}
 	printf("\n");
-	return (0):
+	return (0);
 }
 
