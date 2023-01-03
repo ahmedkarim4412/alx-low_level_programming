2617e2958b6a54837f25530c03688dafca97a7f2
ahmedkarim4412 <ahmedkarim4412@gmail.com>
2023-01-02 16:05:46 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,10 @@
 void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
-	unsigned char *memory = s. value = c;
+	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
 
-	
 	return (memory);
 }
