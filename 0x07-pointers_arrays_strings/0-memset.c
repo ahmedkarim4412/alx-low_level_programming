c10254576912c82a7fde92d69c1786847d0071c1
ahmedkarim4412 <ahmedkarim4412@gmail.com>
2023-01-03 16:50:57 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
- * description _memset: over there 
+ * description _memset: over there.
  * 
  * Return: A pointer to the filled memory area @s
  *
